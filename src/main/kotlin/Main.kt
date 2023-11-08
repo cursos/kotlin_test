4fb4b2d6abb83f52370251303b606d9c2fa7a21f
= <desarrollador4@azsmart.net>
2023-11-08 14:38:03 -0500

    lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,16 @@ const val PI =3.1416
 
 fun main(args: Array<String>) {
 
+    //variablesAndwhen()
+
+    //nullable()
+
+    listas()
+}
+
+
+
+fun variablesAndwhen(){
     println("Hello world")
     var dinero = 10
     println (dinero)
@@ -56,8 +66,67 @@ fun main(args: Array<String>) {
         39,40,41 -> "there are available "
         42,43,44 -> "almost overying"
         45 -> "sorry, we dont have"
-                else-> println("no hay tallas")
+        else-> println("no hay tallas")
     }
     print(mensaje)
+}
+
+fun nullable() {
+    var nombre : String? = null
+
+    try{
+        nombre!!.length
+
+    } catch (error: java.lang.NullPointerException){
+        println("Ha ocurrido un error")
+    }finally {
+        println("Finalmente ha ocurrido un error... Cerrado aplicacion")
+    }
+
+    val primerValor = 10
+    val segundoValor = 0
+    val resultado :Int = try { primerValor/ segundoValor }catch (error: Exception){0}
+    println(resultado)
+
+    var nombre2:String? = null
+    val caracterDeNombre : Int = nombre2?.length ?: 0
+    print(caracterDeNombre)
+
 
 }
+
+fun listas(){
+
+    val listNombres = listOf ("Carlos", "Juan", "Camila")
+    println(listNombres)
+
+    val listaVacia = mutableListOf<String>()
+    println(listaVacia)
+
+    listaVacia.add("Juan")
+    println(listaVacia)
+
+    val valorUsandoGet = listaVacia.get(0);
+    println(valorUsandoGet)
+
+    val valorUsandoOperador = listaVacia[0]
+    println(valorUsandoOperador)
+
+    val primerValor = listNombres.firstOrNull()
+    println(primerValor)
+
+    listaVacia.removeAt(0)
+    println(listaVacia)
+
+    listaVacia.add("Enrique")
+    println(listaVacia)
+    listaVacia.removeIf{caracteres -> caracteres.length >3}
+    println(listaVacia)
+
+    val myArray = arrayOf(1,2,3,4)
+    println("myArray $myArray")
+    println("myArray como lista ${myArray.toList()}")
+
+
+}
+
